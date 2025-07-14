9c1c557ec2d145ea72e45380180754e9b5d4444b
CameronP04 <chpearson@my.waketech.edu>
2025-07-14 17:59:21 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// Stub function to fetch grade data
+function fetchGradeData() {
+    console.log("fetchGradeData() called.");
+    // Future: fetch data from PostgreSQL
+}
+
+// Stub function to populate the table with grade data
+function populateGradebook() {
+    console.log("populateGradebook() called.");
+    // Future: use data from fetchGradeData to fill the table
+}
+
+// Call stubs for testing
+fetchGradeData();
+populateGradebook();
